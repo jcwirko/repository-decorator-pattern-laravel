47b6e95bcb018fec4c340a77175337585c9a6fe8
juanblariza <juanblariza@gmail.com>
2021-01-26 11:37:41 -0300

    Cambia el seeder de usuario

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Car;
-use App\Models\Ticket;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -11,18 +9,6 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        User::create([
-            'first_name' => 'Diego',
-            'last_name' => 'Perez',
-            'email' => 'diegoperez@gmail.com'
-        ]);
-
-        User::create([
-            'first_name' => 'Alonso',
-            'last_name' => 'Diego',
-            'email' => 'diegoalonso@gmail.com'
-        ]);
-
-        User::factory()->times(10000)->create();
+        User::factory()->times(30000)->create();
     }
 }
